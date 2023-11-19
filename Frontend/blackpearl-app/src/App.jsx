def28ef82105cceb5b91110d79cc11b7dc2316af
Mac <sophanpa@gmail.com>
2023-11-19 19:08:42 +0100

    Edit

--- Frontend/blackpearl-app/src/App.jsx
+++ Frontend/blackpearl-app/src/App.jsx
@@ -10,15 +10,22 @@ function App() {
     <>
       <div id='app'>
         <header>
+          <div id='header_logo'>
           <img id='logo_background' src="src/assets/leafs.jpg" alt="" />
-          <nav>
+          
+                    <nav>
             <button>Home</button>
             <button>Bubble Teas</button>
+            <img id='logo' src="src/assets/logo.jpg" alt="" />
             <button>Location</button>
+            <button>About Us</button>
           </nav>
+          </div>
+
+
         </header>
 
-        <main>brooo</main>
+        <main><p>Welcome by Black Pearl</p></main>
         <footer></footer>
       </div>
      
